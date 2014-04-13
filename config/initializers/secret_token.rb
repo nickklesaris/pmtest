bd3275b5e659834fd8700b3811d3615e7d639e4d
Nick Klesaris <nickklesaris@gmail.com>
2014-04-13 17:16:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pmtest::Application.config.secret_key_base = '28c786b47791014661f266d018aae4167a5354c6f76ca854e6568ed6ead49e60f2d2b18a432d3a549ee3001f07e7f77f366119935e7eff052d823e6998b6dba8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pmtest::Application.config.session_store :cookie_store, key: '_pmtest_session'
